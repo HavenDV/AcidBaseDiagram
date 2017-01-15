5703d1c5696c16af6b6158214aa40a3ccc1e4db4
Konstantin S. <havendv@gmail.com>
2017-01-16 01:15:28 +1000

    Added first test

---
+++ AcidBaseLibrary.Tests/Diagram.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AcidBaseLibrary.Tests
+{
+    [TestFixture]
+    public class Diagram_Tests
+    {
+        [Test]
+        public void Diagram_Center()
+        {
+            var data = Diagram.GetData(40, 0);
+            Assert.AreEqual(7.4, data.Item1);
+            Assert.AreEqual(24, data.Item2);
+        }
+    }
+}
---
+++ AcidBaseLibrary/Diagram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcidBaseLibrary
+{
+    public class Diagram
+    {
+        static public Tuple<double, int> GetData(int pco2, int sbe)
+        {
+            return new Tuple<double, int>(7.4, 24);
+        }
+    }
+}
