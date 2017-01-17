fbe222d8c0d33f77a02a2d40ed4574cdba5ef9ef
Konstantin S. <havendv@gmail.com>
2017-01-17 11:48:34 +1000

    Added tests

--- AcidBaseLibrary.Tests/Diagram.Tests.cs
+++ AcidBaseLibrary.Tests/Diagram.Tests.cs
@@ -9,9 +9,18 @@ namespace AcidBaseLibrary.Tests
         [Test]
         public void Diagram_Center()
         {
-            var data = Diagram.GetData(40, 0);
-            Assert.AreEqual(7.4, data.Item1);
-            Assert.AreEqual(24, data.Item2);
+            var data = Diagram.GetData(7.40, 40);
+            Assert.AreEqual(0.0, data.Item1);
+            Assert.AreEqual(24.0, data.Item2);
+        }
+
+        [Test]
+        public void Diagram_UnknownRange()
+        {
+            Assert.Throws<ArgumentException>(() => Diagram.GetData(10.0, 40));
+            Assert.Throws<ArgumentException>(() => Diagram.GetData(0.0, 40));
+            Assert.Throws<ArgumentException>(() => Diagram.GetData(7.40, 0));
+            Assert.Throws<ArgumentException>(() => Diagram.GetData(7.40, 200));
         }
     }
 }
